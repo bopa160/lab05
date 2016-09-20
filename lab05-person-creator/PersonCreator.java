ff827a5218265af30da7db17a345e3a6fcb4d6b1
bopa160 <thomasciavattone@mail.adelphi.edu>
2016-09-20 00:35:42 -0400

    Update PersonCreator.java

--- lab05-person-creator/PersonCreator.java
+++ lab05-person-creator/PersonCreator.java
@@ -10,56 +10,117 @@ public class PersonCreator
     // Student's name
     private String name;
     // Student's age
-    private int age;
+    private String age;
     //student's Sex
     private String sex;
     // Student's Height
-    private int height;
+    private String height;
     // Student's Weight
-    private int weight;
+    private String weight;
     // Student's Socal security numbers
     private String SSN;
     // Student's Address
     private String address;
+    //Stundent's phone Number
+    private String phone;
     //Stundent's Birthdate
     private String birthdate;
+    
 
     // Create the student
     public PersonCreator (String firstName, String lastName )
     {
       name= firstName + " " + lastName;
-      age= -1;
+      age= "";
       sex= "";
-      height= -1;
-      weight= -1;
+      height= "";
+      weight= "";
       SSN= "";
       address= "";
-      phoneNumber= "";
+      phone= "";
       birthdate= "";
     }
+    
     public void changeAge(int yearsOld)
     {
-      age= yearsOld;
+      age= yearsOld + "years old";
     }
+    
     public void changeSex(String gender)
     {
       sex= gender;
     }
+    
     public void changeHeight(int inches)
     {
-        height=inches
+        height=inches + "inches";
     }
+    
     public void changeWeight(int pounds)
     {
-        weight=pounds
+        weight=pounds + "lbs";
     }
+    
     public void changeSSN(int beginNum, int middleNum, int endNum)
     {
         //if beginNum is bad, tell user to input something else.
-        if (beginNum < 000 || beginNum >000)
+        if (beginNum < 000 || beginNum > 999)
         {
-            System.err.print("Please use a vaild beginning number")
+            System.err.print("Please use a vaild beginning number");
         }
+        //if middleNum is bad, tell user to input something else.
         else if (middleNum < 00 || middleNum > 99 )
+        {
+            System.err.print("Please use a vaild middle number");
+        }
+        //if endNum is bad, tell user to input something else.
+        else if (endNum < 0000 || endNum > 9999)
+        {
+            System.err.print("Please use a vaild end number");
+        }
+        SSN= beginNum + "-" + middleNum + "-" + endNum;
     }
+    
+    public void changeAddress(String homeNum)
+    {
+        address=homeNum;
+    }
+    
+    public void changePhone(int areaCode, int numBegin, int numEnd)
+    {
+       if (areaCode < 100 || areaCode > 999)
+    {
+      System.err.println("Please use a valid area code");
+    }
+    else if (numBegin < 100 || numBegin > 999)
+    {
+        System.err.println("Please use a valid begin number");
+    } else if (numEnd <1000 || numEnd > 9999)
+    {
+        System.err.println("Please use a valid end number");
+    }
+    phone= areaCode + "-" + numBegin + "-" + numEnd;
+    
+    }
+
+public void changeBirthdate(String month, int date,int year)
+{
+    if ( date < 01 || date > 31)
+    {
+        System.err.println("Please insert correct date");
+    }
+    birthdate= month + " " + date + ", " + year;
+}
+public void printPersoninfo()
+{
+    System.out.println(name);
+    System.out.println(age);
+    System.out.println(sex);
+    System.out.println(height);
+    System.out.println(weight);
+    System.out.println(SSN);
+    System.out.println(address);
+    System.out.println(phone);
+    System.out.println(birthdate);
+}
 }
